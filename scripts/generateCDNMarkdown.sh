d4ab3689f2998234ab80db6ab058450ae113cca8
rtsfred3 <rtsfred3@gmail.com>
2025-10-01 00:09:05 -0700

    Updates

--- scripts/generateCDNMarkdown.sh
+++ scripts/generateCDNMarkdown.sh
@@ -6,8 +6,8 @@ processFile() {
 	GH="[GH](https://raw.githubusercontent.com/rtsfred3/Adlists/main/$FILE)"
 	GL="[GL](https://gitlab.com/rtsfred3/Adlists/-/raw/main/$FILE)"
 	JSDELIVR="[JSDelivr](https://cdn.jsdelivr.net/gh/rtsfred3/Adlists@main/$FILE)"
-	STATICALLY="[Statically (GitHub)](https://cdn.statically.io/gh/rtsfred3/Adlists/main/$FILE)"
-	STATICALLYGITLAB="[Statically (GitLab)](https://cdn.statically.io/gl/rtsfred3/Adlists/main/$FILE)"
+	STATICALLY="[Statically (GH)](https://cdn.statically.io/gh/rtsfred3/Adlists/main/$FILE)"
+	STATICALLYGITLAB="[Statically (GL)](https://cdn.statically.io/gl/rtsfred3/Adlists/main/$FILE)"
 
 	echo "| $SHORT_FILE | $GH $GL $JSDELIVR $STATICALLY $STATICALLYGITLAB | $(date -r $FILE "+%Y-%m-%d %H:%M:%S") |" >> CDNs.md
 }
