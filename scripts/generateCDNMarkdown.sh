57c436dc7fe431b4992465ff0a29f4b2ef5bd092
rtsfred3 <rtsfred3@gmail.com>
2025-09-30 23:57:16 -0700

    Update generateCDNMarkdown.sh

--- scripts/generateCDNMarkdown.sh
+++ scripts/generateCDNMarkdown.sh
@@ -7,8 +7,9 @@ processFile() {
 	GL="[GL](https://gitlab.com/rtsfred3/Adlists/-/raw/main/$FILE)"
 	JSDELIVR="[JSDelivr](https://cdn.jsdelivr.net/gh/rtsfred3/Adlists@main/$FILE)"
 	STATICALLY="[Statically](https://cdn.statically.io/gh/rtsfred3/Adlists/main/$FILE)"
+	STATICALLYGITLAB="[Statically (GitLab)](https://cdn.statically.io/gl/rtsfred3/Adlists/main/$FILE)"
 
-	echo "| $SHORT_FILE | $GH $GL $JSDELIVR $STATICALLY | $(date -r $FILE "+%Y-%m-%d %H:%M:%S") |" >> CDNs.md
+	echo "| $SHORT_FILE | $GH $GL $JSDELIVR $STATICALLY $STATICALLYGITLAB | $(date -r $FILE "+%Y-%m-%d %H:%M:%S") |" >> CDNs.md
 }
 
 echo "| File | Link | Last Modified |
